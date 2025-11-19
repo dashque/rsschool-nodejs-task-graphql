29e1239c7b8d0aad04b7122db4bc5be2f5c1ccb6
zagorky <zagorkiynahyi@gmail.com>
2025-11-19 21:25:08 +0300

    feat: add Post GraphQL object type with required fields

---
+++ src/routes/graphql/types/profile.type.ts
@@ -0,0 +1,18 @@
+import {
+  GraphQLBoolean,
+  GraphQLInt,
+  GraphQLNonNull,
+  GraphQLObjectType,
+} from 'graphql/index.js';
+import { UUIDType } from './uuid.js';
+import { MemberType } from './member.type.js';
+
+export const Profile = new GraphQLObjectType({
+  name: 'Profile',
+  fields: () => ({
+    id: { type: new GraphQLNonNull(UUIDType) },
+    isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
+    yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
+    memberType: { type: new GraphQLNonNull(MemberType) },
+  }),
+});
