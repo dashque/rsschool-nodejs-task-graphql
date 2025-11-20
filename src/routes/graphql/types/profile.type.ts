6b489c4dedbfd8fbfec8c3b94277de652a95dd37
zagorky <zagorkiynahyi@gmail.com>
2025-11-20 11:23:56 +0300

    feat: add resolver for memberType in Profile GraphQL type

--- src/routes/graphql/types/profile.type.ts
+++ src/routes/graphql/types/profile.type.ts
@@ -13,6 +13,16 @@ export const Profile = new GraphQLObjectType({
     id: { type: new GraphQLNonNull(UUIDType) },
     isMale: { type: new GraphQLNonNull(GraphQLBoolean) },
     yearOfBirth: { type: new GraphQLNonNull(GraphQLInt) },
-    memberType: { type: new GraphQLNonNull(MemberType) },
+    memberType: {
+      type: new GraphQLNonNull(MemberType),
+      resolve: async (parent, _, ctx) => {
+        const profile = await ctx.prisma.profile.findUnique({
+          where: { id: parent.id },
+          include: { memberType: true },
+        });
+
+        return profile?.memberType;
+      },
+    },
   }),
 });
