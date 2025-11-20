358d524bfbdffafd68d5a0991be029c254f18468
zagorky <zagorkiynahyi@gmail.com>
2025-11-20 13:34:15 +0300

    refactor: simplify resolvers by replacing `ctx` with destructured `prisma` argument in GraphQL schema

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,6 +1,12 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { graphql, GraphQLList, GraphQLNonNull, GraphQLObjectType, GraphQLSchema, } from 'graphql';
+import {
+  graphql,
+  GraphQLList,
+  GraphQLNonNull,
+  GraphQLObjectType,
+  GraphQLSchema,
+} from 'graphql';
 import { MemberType, MemberTypeId } from './types/member.type.js';
 import { changeUserInput, createUserInput, User } from './types/user.type.js';
 import { changePostInput, createPostInput, Post } from './types/post.type.js';
@@ -13,52 +19,52 @@ const schema = new GraphQLSchema({
     fields: {
       memberTypes: {
         type: new GraphQLNonNull(new GraphQLList(MemberType)),
-        resolve: async (source, _, ctx) => ctx.prisma.memberType.findMany(),
+        resolve: async (source, _, { prisma }) => prisma.memberType.findMany(),
       },
       memberType: {
         type: MemberType,
         args: {
           id: { type: new GraphQLNonNull(MemberTypeId) },
         },
-        resolve: async (source, { id }, ctx) =>
-          ctx.prisma.memberType.findUnique({ where: { id } }),
+        resolve: async (source, { id }, { prisma }) =>
+          prisma.memberType.findUnique({ where: { id } }),
       },
       users: {
         type: new GraphQLNonNull(new GraphQLList(User)),
-        resolve: async (source, _, ctx) => ctx.prisma.user.findMany(),
+        resolve: async (source, _, { prisma }) => prisma.user.findMany(),
       },
       user: {
         type: User,
         args: {
           id: { type: new GraphQLNonNull(UUIDType) },
         },
-        resolve: async (source, { id }, ctx) =>
-          ctx.prisma.user.findUnique({ where: { id } }),
+        resolve: async (source, { id }, { prisma }) =>
+          prisma.user.findUnique({ where: { id } }),
       },
 
       posts: {
         type: new GraphQLNonNull(new GraphQLList(Post)),
-        resolve: async (source, _, ctx) => ctx.prisma.post.findMany(),
+        resolve: async (source, _, { prisma }) => prisma.post.findMany(),
       },
       post: {
         type: Post,
         args: {
           id: { type: new GraphQLNonNull(UUIDType) },
         },
-        resolve: async (source, { id }, ctx) =>
-          ctx.prisma.post.findUnique({ where: { id } }),
+        resolve: async (source, { id }, { prisma }) =>
+          prisma.post.findUnique({ where: { id } }),
       },
       profiles: {
         type: new GraphQLNonNull(new GraphQLList(Profile)),
-        resolve: async (source, _, ctx) => ctx.prisma.profile.findMany(),
+        resolve: async (source, _, { prisma }) => prisma.profile.findMany(),
       },
       profile: {
         type: Profile,
         args: {
           id: { type: new GraphQLNonNull(UUIDType) },
         },
-        resolve: async (source, { id }, ctx) =>
-          ctx.prisma.profile.findUnique({ where: { id } }),
+        resolve: async (source, { id }, { prisma }) =>
+          prisma.profile.findUnique({ where: { id } }),
       },
     },
   }),
@@ -70,21 +76,21 @@ const schema = new GraphQLSchema({
         args: {
           dto: { type: new GraphQLNonNull(createUserInput) },
         },
-        resolve: async (_, { dto }, ctx) => ctx.prisma.user.create({ data: dto }),
+        resolve: async (_, { dto }, { prisma }) => prisma.user.create({ data: dto }),
       },
       createProfile: {
         type: Profile,
         args: {
           dto: { type: new GraphQLNonNull(createProfileInput) },
         },
-        resolve: async (_, { dto }, ctx) => ctx.prisma.profile.create({ data: dto }),
+        resolve: async (_, { dto }, { prisma }) => prisma.profile.create({ data: dto }),
       },
       createPost: {
         type: Post,
         args: {
           dto: { type: new GraphQLNonNull(createPostInput) },
         },
-        resolve: async (_, { dto }, ctx) => ctx.prisma.post.create({ data: dto }),
+        resolve: async (_, { dto }, { prisma }) => prisma.post.create({ data: dto }),
       },
       changePost: {
         type: Post,
@@ -92,8 +98,8 @@ const schema = new GraphQLSchema({
           id: { type: new GraphQLNonNull(UUIDType) },
           dto: { type: new GraphQLNonNull(changePostInput) },
         },
-        resolve: async (_, { id, dto }, ctx) =>
-          ctx.prisma.post.update({ where: { id }, data: dto }),
+        resolve: async (_, { id, dto }, { prisma }) =>
+          prisma.post.update({ where: { id }, data: dto }),
       },
       changeProfile: {
         type: Profile,
@@ -101,8 +107,8 @@ const schema = new GraphQLSchema({
           id: { type: new GraphQLNonNull(UUIDType) },
           dto: { type: new GraphQLNonNull(changeProfileInput) },
         },
-        resolve: async (_, { id, dto }, ctx) =>
-          ctx.prisma.profile.update({ where: id, data: dto }),
+        resolve: async (_, { id, dto }, { prisma }) =>
+          prisma.profile.update({ where: id, data: dto }),
       },
       changeUser: {
         type: User,
@@ -110,8 +116,8 @@ const schema = new GraphQLSchema({
           id: { type: new GraphQLNonNull(UUIDType) },
           dto: { type: new GraphQLNonNull(changeUserInput) },
         },
-        resolve: async (_, { id, dto }, ctx) =>
-          ctx.prisma.user.update({ where: id, data: dto }),
+        resolve: async (_, { id, dto }, { prisma }) =>
+          prisma.user.update({ where: id, data: dto }),
       },
       // deleteUser: {},
       // deletePost: {},
