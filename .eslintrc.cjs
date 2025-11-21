7998db07fddd664e2b406a40b8f2d3a8d96f3f94
zagorky <zagorkiynahyi@gmail.com>
2025-11-21 11:45:06 +0300

    chore: disable unsafe TypeScript rules in ESLint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -20,6 +20,10 @@ module.exports = {
   rules: {
     '@typescript-eslint/require-await': 0,
     '@typescript-eslint/ban-ts-comment': 0,
+    '@typescript-eslint/no-unsafe-assignment': 'off',
+    '@typescript-eslint/no-unsafe-return': 'off',
+    '@typescript-eslint/no-unsafe-call': 'off',
+    '@typescript-eslint/no-unsafe-member-access': 'off',
     '@typescript-eslint/no-misused-promises': [
       2,
       {
